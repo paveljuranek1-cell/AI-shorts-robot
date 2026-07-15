d8fa1a82977c36c3753bffae0a1ef2885c5b75fa
paveljuranek1-cell <pavel.juranek1@gmail.com>
2026-07-15 22:29:54 +0200

    Integrate Gemini API for YouTube Shorts generation
    Added functionality to generate YouTube Shorts scripts using Gemini API.

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
+import os
+import google.generativeai as genai
 from datetime import datetime
 
-print("================================")
+
+print("============================")
 print("AI Shorts Robot spuštěn")
-print("Čas spuštění:", datetime.now())
-print("================================")
+print(datetime.now())
+print("============================")
 
-# Tady později přidáme:
-# 1. Gemini vytvoří scénáře
-# 2. AI vytvoří hlas
-# 3. Vytvoří se video
-# 4. Video se nahraje na YouTube
 
-print("Robot dokončil test.")
+# načtení API klíče
+api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    print("Chybí GEMINI_API_KEY")
+    exit()
+
+
+genai.configure(
+    api_key=api_key
+)
+
+
+model = genai.GenerativeModel(
+    "gemini-2.0-flash"
+)
+
+
+prompt = """
+Jsi expert na YouTube Shorts.
+
+Vytvoř 4 virální krátká videa.
+
+Pro každé napiš:
+
+- Název
+- Hook první 3 sekundy
+- Scénář na 45 sekund
+- Návrh scén
+
+Téma:
+zajímavá fakta
+"""
+
+
+response = model.generate_content(prompt)
+
+
+print("============================")
+print("GENEROVANÉ SCÉNÁŘE")
+print("============================")
+
+print(response.text)
+
+
+print("============================")
+print("Hotovo")
+print("============================")
