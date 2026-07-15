02334deb87325f073480dfc876dec7df5a90240f
paveljuranek1-cell <pavel.juranek1@gmail.com>
2026-07-15 22:55:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
 import os
-from datetime import datetime
-from google import genai
 
+key = os.getenv("GEMINI_API_KEY")
 
-print("============================")
-print("AI Shorts Robot spuštěn")
-print(datetime.now())
-print("============================")
-
-
-api_key = os.getenv("GEMINI_API_KEY")
-
-if not api_key:
-    print("Chybí API klíč")
-    exit()
-
-
-client = genai.Client(
-    api_key=api_key
-)
-
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="""
-Vytvoř 4 virální YouTube Shorts scénáře.
-
-Pro každý napiš:
-- název
-- hook první 3 sekundy
-- scénář na 45 sekund
-- návrh scén
-
-Téma:
-zajímavá fakta
-"""
-)
-
-
-print("============================")
-print(response.text)
-print("============================")
+if key:
+    print("API klíč nalezen ✅")
+else:
+    print("API klíč chybí ❌")
